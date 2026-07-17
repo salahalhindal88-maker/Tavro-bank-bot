f06184c851befc6bfc80ff4749f95b0fd93bad14
salahalhindal88-maker <salah.alhindal88@gmail.com>
2026-07-18 01:25:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -964,7 +964,7 @@ if (cmd==='تداول') {
   let rawChange, result;
 
   // 25% موجة صعود | 25% ارتفاع بسيط | 25% تراجع | 25% انهيار
-  if(roll>0.63){
+  if(roll>0.65){
     rawChange=Math.floor(amount*(0.3+Math.random()*0.7));
     result='📈 موجة صعود!';
   }
